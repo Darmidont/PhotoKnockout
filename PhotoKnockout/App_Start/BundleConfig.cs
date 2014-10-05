a8f38d7bf887d66fb290589de6c4e1ce44b7fcc7
Darmidont <dara_r@rambler.ru>
2014-10-05 19:04:03 +0300

    Running project

---
+++ PhotoKnockout/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PhotoKnockout.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/base")
+                               .Include("~/Scripts/jquery-2.1.1.js")
+                               .Include("~/Scripts/knockout-3.2.0.js")
+                               .Include("~/Scripts/bootstrap.js")
+                               .Include("~/Scripts/Common/knockout.validation.js"));
+        }
+    }
+}
